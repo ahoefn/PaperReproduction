b27014e08926b7d603814e32a3b1eb20bbeb7961
Arno Hoefnagels <hoefnagelsarno@gmail.com>
2025-05-24 12:34:54 +0200

    added saving and loading capabilities for models and trainers

--- SubgraphExplain/main.py
+++ SubgraphExplain/main.py
@@ -8,16 +8,54 @@ from mutag_model import MUTAGModel
 from trainer import Trainer
 
 
-if __name__ == "__main__":
-    mutag_data = MUTAGDataLoader()
+def train_model(name: str, epochs: int, mutag_data: MUTAGDataLoader) -> Trainer:
     model = MUTAGModel()
 
     trainer = Trainer(model, mutag_data)
-    trainer.step(2000)
+    trainer.step(epochs)
+    trainer.save_model(name)
 
-    print(f"Final loss is: {trainer.train_loss[-1]}")
-    print(f"Final accuracy is: {trainer.train_accuracy[-1]}")
+    return trainer
 
-    trainer.plot_losses()
-    trainer.plot_accuracy()
+
+def load_model(name: str, mutag_data: MUTAGDataLoader) -> Trainer:
+    model = MUTAGModel()
+    trainer = Trainer(model, mutag_data)
+    trainer.load_model(name)
+    return trainer
+
+
+if __name__ == "__main__":
+
+    data = MUTAGDataLoader()
+    trainer = train_model("test", 10, data)
+    trainer_loaded = load_model("test", data)
+
+    # check if model paramaters are indeed the same
+    for trained_params, loaded_params in zip(
+        trainer.model.parameters(), trainer_loaded.model.parameters()
+    ):
+        if trained_params.ne(loaded_params).sum() > 0:
+            print(
+                f"Error: not all parameters are equal. Problem in {trained_params} and {loaded_params}"
+            )
+
+    fig_loss = plt.figure(
+        figsize=(
+            12,
+            12,
+        )
+    )
+    trainer.plot_losses(fig_loss)
+    trainer_loaded.train_loss = [x + 0.1 for x in trainer_loaded.train_loss]
+    trainer_loaded.plot_losses(fig_loss)
+
+    fig_accuracy = plt.figure(
+        figsize=(
+            12,
+            12,
+        )
+    )
+    trainer.plot_accuracy(fig_accuracy)
+    trainer_loaded.plot_accuracy(fig_accuracy)
     plt.show()
